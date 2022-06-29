2c0dce40b6151ca75231cf97cc9ea0c5a8dcf2b5
Samuel Arguello <sam.rene12@gmail.com>
2022-06-29 12:37:26 -0500

    feat: created initial fetch request got mudkip json obj to display in console

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 function App() {
+  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/mudkip/";
+
+  fetch(pokemonUrl)
+    .then((response) => response.json())
+    .then((mudkip) => console.log(mudkip))
+    .catch((error) => console.log(error));
+
   return (
     <div>
       <h2>Hello world</h2>
