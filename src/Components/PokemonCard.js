e0d3adac9961cc5d4119f612acbf06b7aad51582
Samuel Arguello <sam.rene12@gmail.com>
2022-06-30 17:18:00 -0500

    feat: pokemon with multiple types will now have both displayed within their card

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -5,6 +5,9 @@ const PokemonCard = (props) => {
     <div>
       <img src={props.sprites.front_default} />
       <h3>{props.name}</h3>
+      {props.types.map((type) => {
+        return <p>{type.type.name}</p>;
+      })}
     </div>
   );
 };
