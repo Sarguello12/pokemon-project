dfbaeea75676adf39a8448e31a37aaee738aa8b1
Samuel Arguello <sam.rene12@gmail.com>
2022-06-30 16:36:14 -0500

    fix: cut out bloat code and unused functions replaced by promise refactor

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -4,14 +4,12 @@ import { getPokemon, getPokemonDetails } from "../services/GetPokemon";
 
 const PokemonList = () => {
   const [pokemon, setPokemon] = useState([]);
-  const [loading, isLoading] = useState(true);
   const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
   useEffect(() => {
     const fetchPokemon = async () => {
       let response = await getPokemonDetails(URL);
       await populatePokemon(response.results);
-      isLoading(false);
     };
     fetchPokemon();
     // Notice the empty array following the useEffect call. Note that the useEffect runs for both a component mounting and updating.
@@ -31,9 +29,6 @@ const PokemonList = () => {
   // state updates happen asynchronously, meaning that updating state does not stop the rest of the application from rendering and running smoothly.
   // So to fix this error, we have to do something called short-circuiting(&&).
   // Basically, we have to first check if the pokemon object is populated and then we can call to it.
-  console.log(pokemon);
-  // console.log(pokemon.name);
-
   return (
     <div>
       <div>
--- src/services/GetPokemonDetails.js
+++
@@ -1,9 +0,0 @@
-// export const getPokemonDetails = (pokemon) => {
-//   let url = pokemon.url;
-
-//   return fetch(url)
-//     .then((response) => response.json())
-//     .then((pokemonData) => {
-//       // console.log(pokemonData);
-//     });
-// };
