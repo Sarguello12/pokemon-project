beecabbb2715d9fa92e7e7794554aebe1813aba4
Samuel Arguello <sam.rene12@gmail.com>
2022-07-01 17:17:46 -0500

    TODO: began work to dynamicly chnage the color of the types to the matching color

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./PokemonCard.css";
+import { getTypeColor } from "../services/DisplayType";
 
 const PokemonCard = (props) => {
   return (
     <div className="pokemon-card">
       <img src={props.sprites.front_default} />
       <h3>{props.name}</h3>
+      <p>#{props.id}</p>
       {props.types.map((type) => {
         return <p>{type.type.name}</p>;
       })}
