51661478895dd81bc46272f0dfb3f9effb8ac51c
Samuel Arguello <sam.rene12@gmail.com>
2022-07-03 11:45:19 -0500

    fix: reactored PokemonTypes to handling mapping logic

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -9,16 +9,7 @@ const PokemonCard = (props) => {
       <img src={props.sprites.front_default} />
       <h3>{props.name}</h3>
       <PokemonNum num={props.id} />
-      {props.types.map((type, index) => {
-        // return <PokemonTypes key={index} type={type.type.name} />;
-        return (
-          <div>
-            <p key={index} className={type.type.name}>
-              {type.type.name}
-            </p>
-          </div>
-        );
-      })}
+      <PokemonTypes types={props.types} />
     </div>
   );
 };
--- src/Components/PokemonTypes.js
+++ src/Components/PokemonTypes.js
@@ -1,89 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const PokemonTypes = (props) => {
-  console.log(props.type);
-  let pokemonsType = props.type;
-
-  const [pokemonType, setPokemonType] = useState(pokemonsType);
-
-  useEffect(() => {
-    switch (pokemonType) {
-      case "fire":
-        setPokemonType("fire");
-        console.log("fire");
-        break;
-      case "water":
-        setPokemonType("water");
-        console.log("water");
-        break;
-      case "grass":
-        setPokemonType("grass");
-        console.log("grass");
-        break;
-      case "normal":
-        setPokemonType("normal");
-        console.log("normal");
-        break;
-      case "electric":
-        setPokemonType("electric");
-        console.log("electric");
-        break;
-      case "ice":
-        setPokemonType("ice");
-        console.log("ice");
-        break;
-      case "fighting":
-        setPokemonType("fighting");
-        console.log("fighting");
-        break;
-      case "poison":
-        setPokemonType("poison");
-        console.log("poison");
-        break;
-      case "ground":
-        setPokemonType("ground");
-        console.log("ground");
-        break;
-      case "flying":
-        setPokemonType("flying");
-        console.log("flying");
-        break;
-      case "psychic":
-        setPokemonType("psychic");
-        console.log("psychic");
-        break;
-      case "bug":
-        setPokemonType("bug");
-        console.log("bug");
-        break;
-      case "rock":
-        setPokemonType("rock");
-        console.log("rock");
-        break;
-      case "ghost":
-        setPokemonType("ghost");
-        console.log("ghost");
-        break;
-      case "dark":
-        setPokemonType("dark");
-        console.log("dark");
-        break;
-      case "dragon":
-        setPokemonType("dragon");
-        console.log("dragon");
-        break;
-      case "steel":
-        setPokemonType("steel");
-        console.log("steel");
-        break;
-      case "fairy":
-        setPokemonType("fairy");
-        console.log("fairy");
-        break;
-    }
-  });
-
-  return <p className={pokemonType}>{pokemonType}</p>;
+  return (
+    <ul>
+      {props.types.map((type, index) => {
+        return (
+          <div>
+            <p key={index} className={type.type.name}>
+              {type.type.name}
+            </p>
+          </div>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default PokemonTypes;
